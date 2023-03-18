ad9d317658fc0041267213818e727769d13e13a0
ganjunhao <gan879065613@163.com>
2023-03-18 16:03:17 +0800

    format

--- common/src/main/java/com/ganjunhao/pojo/dto/ExcelImportDTO.java
+++ common/src/main/java/com/ganjunhao/pojo/dto/ExcelImportDTO.java
@@ -3,7 +3,6 @@ package com.ganjunhao.pojo.dto;
 import lombok.Data;
 import org.springframework.web.multipart.MultipartFile;
 
-import javax.validation.constraints.NotNull;
 
 /**
  * Excel文件导入DTO
@@ -11,7 +10,6 @@ import javax.validation.constraints.NotNull;
 @Data
 public class ExcelImportDTO {
 
-    @NotNull(message = "导入EXCEL不能为空")
     private MultipartFile file;
 
 }
